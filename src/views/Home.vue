97b13d77fc726242359ebacae3d0c2abd12935c1
康兵奎 <178974662@qq.com>
2019-05-10 15:30:36 +0800

    增加滚动条组件&右键菜单组件&Demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,98 @@
 <template>
     <div class="v-home">
-        首页sss
+        <div class="v-title">滚动条-y</div>
+        <ScrollBar class="v-container">
+            <div class="line-text">测试数据-1</div>
+            <div class="line-text">测试数据-2</div>
+            <div class="line-text">测试数据-3</div>
+            <div class="line-text">测试数据-4</div>
+            <div class="line-text">测试数据-5</div>
+            <div class="line-text">测试数据-6</div>
+            <div class="line-text">测试数据-7</div>
+            <div class="line-text">测试数据-8</div>
+            <div class="line-text">测试数据-9</div>
+            <div class="line-text">测试数据-10</div>
+            <div class="line-text">测试数据-11</div>
+            <div class="line-text">测试数据-12</div>
+            <div class="line-text">测试数据-13</div>
+        </ScrollBar>
+        <div class="v-title">滚动条-x</div>
+        <ScrollBar class="v-container" axis="x">
+            <div class="line-text long-width">测试数据.....</div>
+        </ScrollBar>
+        <div class="v-title">右键菜单</div>
+        <MouseRight class="right-mouse-el" :mouseData="mouseData" :mouseKey="value">
+            <div class="mouse-right-value">右键点击</div>
+        </MouseRight>
     </div>
 </template>
 
 <script>
+import ScrollBar from '@components/ScrollBar';
+import MouseRight from '@components/MouseRight';
 export default {
     name: 'Home',
+    components: {
+        ScrollBar,
+        MouseRight,
+    },
+    data: function() {
+        return {
+            mouseData: [{
+                text: 'alert',
+                action: (val) => {
+                    alert(JSON.stringify(val));
+                },
+            }, {
+                text: 'alert',
+                action: (val) => {
+                    alert(JSON.stringify(val));
+                },
+            }],
+            value: {
+                id: 1,
+                text: '1',
+            },
+        };
+    },
 };
 </script>
 
 <style lang="less" scoped>
 .v-home {
     font-size: 20px;
+    .v-title {
+        .font-size-lg();
+        color: @primary;
+        .m(@gap-md);
+    }
+    .v-container {
+        .m(@gap-md);
+        height: 200px;
+        background-color: @gray;
+        .font-size-sm();
+        .line-text {
+            .font-size();
+            text-indent: @gap;
+            .m-v(@gap-sm);
+        }
+        .long-width {
+            width: 1000px;
+            background-color: @warn;
+        }
+    }
+    .right-mouse-el {
+        .m(@gap-md);
+        width: 100%;
+        .p(@gap);
+        .flex-row-center();
+        background-color: @gray;
+        .mouse-right-value {
+            background-color: @danger;
+            color: @white;
+            width: 100px;
+            .t-c();
+        }
+    }
 }
 </style>
