a65a7c2defc3b59d907112d50ac406e30cb757eb
康兵奎 <BingKui@users.noreply.github.com>
2019-09-07 11:41:40 +0800

    优化&升级依赖库
    1.electron 升级到 v6
    2.其他依赖库版本升级到最新

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: {
         'autoprefixer': {
-            browsers: ['last 5 version', 'Android >= 4.0'],
+            overrideBrowserslist: ['last 5 version', 'Android >= 4.0'],
             //是否美化属性值 默认：true
             cascade: true,
             //是否去掉不必要的前缀 默认：true
